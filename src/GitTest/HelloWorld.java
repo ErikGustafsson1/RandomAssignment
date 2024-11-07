5ea2bdd708882f4dc896c60e9606f9034a031ce2
ErikGustafsson1 <erik.gustafsson22@gmail.com>
2024-11-07 16:47:47 +0100

    Hello World

--- src/GitTest/HelloWorld.java
+++ src/GitTest/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Cats!");
         System.out.println("Hello Dogs!");
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!! sup ");
     }
 }
